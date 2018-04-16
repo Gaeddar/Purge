89afc81bb54e1e036d7eb4e92108e3aa7954398a
Gaeddar <ew90Coke@hotmail.com>
2018-04-16 16:24:07 +0200

    first commit
    initial release

---
+++ README.md
@@ -0,0 +1,14 @@
+# Purge
+
+Based on "dawn" by 0-l (https://github.com/0-l/dawn)
+
+A startpage called "Purge"
+
+Press `alt-<n>` to change tabs.
+
+[**Live Preview**](https://gaeddar.github.io/Purge/?)
+
+### Preview
+![preview](preview.png)
+
+disclaimer! I've only tweaked the original a tiny bit to fit my needs
---
+++ js/powerline.js
@@ -0,0 +1,31 @@
+// +-----------+
+// | POWERLINE |
+// +-----------+
+class Powerline {
+  constructor() {
+    this.tabs = {
+      1: 'home',
+      2: 'music',
+      3: 'crypto',
+	  4: 'secret'
+    };
+  }
+
+  get on() {
+    document.onkeydown = (e) => {
+      if (e.altKey && e != undefined) {
+        if (Number.isInteger(parseInt(e.key))) {
+          this.activate('#tabs ul li', `#tabs ul li:nth-child(${e.key})`);
+          this.activate('#panels ul', `.${this.tabs[e.key]}`);
+        }
+      }
+    };
+
+    $.qS('.time').innerHTML = `${strftime('H')}<span>:${strftime('i')}</span>`;
+  }
+
+  activate(obj, item) {
+    $.qA(obj).forEach((i) => i.removeAttribute('active'));
+    $.qS(item).setAttribute('active', '');
+  };
+}
